cb8fbf9120c5d97f03aa5fc26ae16034388ef6df
Yodaman07 <76266292+Yodaman07@users.noreply.github.com>
2022-10-16 15:25:49 -0700

    Fixed issue: was running mod on the server

--- src/main/java/legobrosbuild/coordsender/CoordSender.java
+++ src/main/java/legobrosbuild/coordsender/CoordSender.java
@@ -1,30 +1,10 @@
 package legobrosbuild.coordsender;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.command.v1.CommandRegistrationCallback;
-import net.minecraft.client.util.math.Vector3d;
-import net.minecraft.entity.Entity;
-import net.minecraft.text.LiteralText;
-import net.minecraft.text.Text;
-import net.minecraft.util.Formatting;
 
-import static net.minecraft.server.command.CommandManager.literal;
 
 public class CoordSender implements ModInitializer {
     @Override
     public void onInitialize() {
-        CommandRegistrationCallback.EVENT.register(((dispatcher, dedicated) -> {
-            dispatcher.register(literal("coords")
-                .executes(context -> {
-                    Entity player = context.getSource().getEntity();
-                    assert player != null;
-                    Vector3d coords = new Vector3d(player.getBlockX(), player.getBlockY(), player.getBlockZ());
-                    String pos = coords.x + ", " + coords.y + ", " + coords.z;
-                    final Text text = new LiteralText(player.getEntityName() + ": " + pos).formatted(Formatting.AQUA);
-                    context.getSource().sendFeedback(text, true);
-                    return 1;
-                })
-            );
-        }));
     }
 }
--- src/main/java/legobrosbuild/coordsender/client/CoordSenderClient.java
+++ src/main/java/legobrosbuild/coordsender/client/CoordSenderClient.java
@@ -3,11 +3,32 @@ package legobrosbuild.coordsender.client;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.command.v1.ClientCommandManager;
+import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.util.math.Vector3d;
+import net.minecraft.entity.Entity;
+import net.minecraft.text.LiteralText;
+import net.minecraft.text.Text;
+import net.minecraft.util.Formatting;
+
 
 @Environment(EnvType.CLIENT)
 public class CoordSenderClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
+        ClientCommandManager.DISPATCHER.register(
+            ClientCommandManager.literal("coords")
+                .executes(context -> {
 
+                    ClientPlayerEntity player = context.getSource().getClient().player;
+
+                    assert player != null;
+                    Vector3d coords = new Vector3d(player.getBlockX(), player.getBlockY(), player.getBlockZ());
+                    String pos = coords.x + ", " + coords.y + ", " + coords.z;
+                    final Text text = new LiteralText(player.getEntityName() + ": " + pos).formatted(Formatting.AQUA);
+                    player.sendMessage(text, false);
+                    return 1;
+                })
+        );
     }
 }
